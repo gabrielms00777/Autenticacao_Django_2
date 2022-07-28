ea32c94d658d223bed751172818d49be47d07be3
Gabriel Moreira <gabrielms00777@gmail.com>
2022-07-28 20:09:49 -0300

    Criação de alerts da pagina de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -6,7 +6,8 @@ def login(request):
     return render(request, 'login.html')
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    status = request.GET.get('status')
+    return render(request, 'cadastro.html', {'status':status})
 
 def validar_cadastro(request):
     #Status 0 = Usuário cadastrado com sucesso
@@ -25,7 +26,7 @@ def validar_cadastro(request):
         return redirect('/auth/cadastro/?status=1')
 
     if len(senha) < 8:
-        return redirect('auth/cadastro/?status=2')
+        return redirect('/auth/cadastro/?status=2')
 
     if usuario:
         return redirect('/auth/cadastro/?status=3')
@@ -42,6 +43,23 @@ def validar_cadastro(request):
         return redirect('/auth/cadastro/?status=4')
 
 def validar_login(request):
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+
+    senha = sha256(senha.encode()).hexdigest()
+
+    usuario = Usuarios.objects.filter(email=email).filter(senha=senha)
+
+    if len(usuario) == 0:
+        print(0)
+
+    if len(usuario) > 0:
+        print(1)
+    print(usuario)
+
+
     return render(request, 'login.html')
 
+def sair(request):
+    return redirect('login')
 
